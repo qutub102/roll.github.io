55c863464e5d430d830f5083db2b9adcb6162733
Qutubuddin Bohra <qutubuddiniam@gmail.com>
2021-05-27 23:07:10 +0530

    small change

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ btnHold.addEventListener('click', function () {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 50) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
